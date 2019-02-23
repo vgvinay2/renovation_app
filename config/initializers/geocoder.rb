cf9e67cacd2b87bf1078af0e9ad7a8b7b5eabbc4
Vinay Gupta <vinaygupta@BRIMACLAP017.local>
2019-02-24 00:38:15 +0530

    Initial setup

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,3 @@
+Geocoder::Configuration.timeout = 15
+# :lookup => :google, # for street addresses
+# :ip_lookup => :freegeoip # for IP addresses
---
+++ db/migrate/20190223130719_create_downloads.rb
@@ -0,0 +1,10 @@
+class CreateDownloads < ActiveRecord::Migration[5.2]
+  def change
+    create_table :downloads do |t|
+      t.string :ip
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
+  end
+end
