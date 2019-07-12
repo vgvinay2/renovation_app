c8fd683509afbedef00f4cf4be6652cc7592387d
Vinay Gupta <vinaygupta@BRIMACLAP017.local>
2019-07-12 11:07:49 +0530

    added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def get_user_role
+    User&.group(:role)&.pluck(:role,:role)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :tickets
+  resources :users
+ root to: 'users#index'
   resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
